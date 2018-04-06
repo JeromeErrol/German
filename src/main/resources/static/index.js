19beeeb129a2893140836539284e25d8455201ea
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2018-04-06 17:04:47 +1000

    blep

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,12 +1,38 @@
+var ui = {
+    setPrimaryTable: function (id) {
+        $("#hidden").append($("#primary-table").children());
+        $("#primary-table").append($(id));
+    },
+    setSubMenu1: function (id) {
+        $("#hidden").append($("#sub-menu").children());
+        $("#sub-menu").append($(id));
+    },
+    setSubMenu2: function (id) {
+        $("#hidden").append($("#sub-menu1").children());
+        $("#sub-menu1").append($(id));
+    }
+}
+
 $(document).ready(function () {
+
+
+    $("#pronoun").click(function () {
+        ui.setPrimaryTable("#pronouns-table")
+    });
+    $("#verb").click(function () {
+        ui.setSubMenu1("#verb-menu");
+        ui.setPrimaryTable("#wollen-table")
+    });
 
     $('.word').on("mouseover", function () {
 
         $(".selected-top-menu").removeClass("selected-top-menu");
         $(".highlighted-word").removeClass("highlighted-word");
         $(".selected-word").removeClass("selected-word");
+        $(".selected-modal-verb").removeClass("selected-modal-verb");
         $("#hidden").append($("#primary-table").children());
         $("#hidden").append($("#sub-menu").children());
+        $("#hidden").append($("#sub-menu2").children());
 
         var hoveredWord = $(this).text();
         $("#highlighted").text(hoveredWord);
@@ -23,7 +49,10 @@ $(document).ready(function () {
             $("#" + verbType).addClass("selected-verb-type")
 
             if (verbType == "verb-modal") {
-                $("#sub-menu").append($("#modal-verbs"));
+                $("#sub-menu2").append($("#modal-verbs"));
+
+                var modelType = $(this).data("modaltype");
+                $("#" + modelType).addClass("selected-modal-verb");
             }
         }
 
