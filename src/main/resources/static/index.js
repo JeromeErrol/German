2a2d44a8eb17cd4daa7cdcfaaa166f8241ecd39b
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2018-04-06 16:29:16 +1000

    blep

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,40 +1,35 @@
 $(document).ready(function () {
 
-    $('.word').on("mouseover", function() {
+    $('.word').on("mouseover", function () {
+
+        $(".selected-top-menu").removeClass("selected-top-menu");
+        $(".highlighted-word").removeClass("highlighted-word");
+        $(".selected-word").removeClass("selected-word");
+        $("#hidden").append($("#primary-table").children());
+        $("#hidden").append($("#sub-menu").children());
+
         var hoveredWord = $(this).text();
         $("#highlighted").text(hoveredWord);
-        $(".selected-word").removeClass("selected-word");
         var lowerCase = hoveredWord.toLowerCase();
         var val = $("#" + lowerCase);
-
-        $(".selected-type").removeClass("selected-type");
+        $(this).addClass("highlighted-word");
         var type = $(this).data("type");
-        $("#" + type).addClass("selected-type");
+        $("#" + type).addClass("selected-top-menu");
 
-        // for now just always hide
-        $("#staging").append($("#modal-verbs"));
-        $("#staging").append($("#verb-menu"));
-
-        if(type == "verb"){
+        if (type == "verb") {
             $("#sub-menu").append($("#verb-menu"));
-
             var verbType = $(this).data("verbtype");
             $(".selected-verb-type").removeClass("selected-verb-type");
             $("#" + verbType).addClass("selected-verb-type")
 
-            if(verbType == "verb-modal"){
+            if (verbType == "verb-modal") {
                 $("#sub-menu").append($("#modal-verbs"));
             }
-
         }
 
-        if(val.length > 0){
+        if (val.length > 0) {
             val.addClass("selected-word");
             var table = val.closest(".guide");
-            var currentTables = $("#primary-table").children();
-            if(currentTables.length > 0){
-                $("#staging").append(currentTables[0]);
-            }
             $("#primary-table").append(table);
         }
     });
