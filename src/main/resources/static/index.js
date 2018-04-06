d273ae0781040d97d645453c0a9a9569fe9a9165
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2018-04-06 15:59:11 +1000

    add logic

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -3,12 +3,39 @@ $(document).ready(function () {
     $('.word').on("mouseover", function() {
         var hoveredWord = $(this).text();
         $("#highlighted").text(hoveredWord);
-
         $(".selected-word").removeClass("selected-word");
         var lowerCase = hoveredWord.toLowerCase();
         var val = $("#" + lowerCase);
-        if(val != null){
+
+        $(".selected-type").removeClass("selected-type");
+        var type = $(this).data("type");
+        $("#" + type).addClass("selected-type");
+
+        // for now just always hide
+        $("#staging").append($("#modal-verbs"));
+        $("#staging").append($("#verb-menu"));
+
+        if(type == "verb"){
+            $("#sub-menu").append($("#verb-menu"));
+
+            var verbType = $(this).data("verbtype");
+            $(".selected-verb-type").removeClass("selected-verb-type");
+            $("#" + verbType).addClass("selected-verb-type")
+
+            if(verbType == "verb-modal"){
+                $("#sub-menu").append($("#modal-verbs"));
+            }
+
+        }
+
+        if(val.length > 0){
             val.addClass("selected-word");
+            var table = val.closest(".guide");
+            var currentTables = $("#primary-table").children();
+            if(currentTables.length > 0){
+                $("#staging").append(currentTables[0]);
+            }
+            $("#primary-table").append(table);
         }
     });
 });
